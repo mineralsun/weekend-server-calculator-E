ed8296c76fbc75e33ae1b2a1c49d0352e5fbd7ad
Everett Butler <everettjamesbutler@gmail.com>
2023-03-03 14:58:41 -0600

    got all basic functions working on client. will begin transfering them to server side so I reference them upon post and get!

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,15 +1,33 @@
-const { response } = require("express");
-
 console.log('Yo waddup');
 
 const num1 = document.querySelector('#firstNumber').value;
 const num2 = document.querySelector('#secondNumber').value;
 
 function addNumbers(num1, num2) {
-    let sum = num1 + num2
+    let sum = num1 + num2;
     return sum;
-}
+};
 console.log(addNumbers(2,3));
+
+function subtractNumbers(num1, num2) {
+    let sum = num1 - num2;
+    return sum;
+};
+console.log(subtractNumbers(5,4));
+
+function multiplyNumbers(num1, num2) {
+    let sum = num1 * num2;
+    return sum;
+};
+console.log(multiplyNumbers(5,3));
+
+function divideNumbers(num1, num2) {
+    let sum = num1 / num2;
+    return sum;
+};
+console.log(divideNumbers(8,2));
+
+
 
 function getMath() {
     axios.get('/math').then((response) => {
@@ -26,7 +44,7 @@ function getMath() {
         console.log(error);
         alert('Something went wrong');
     })
-}
+};
 
 function submitEquation(event) {
     event.preventDefault();
