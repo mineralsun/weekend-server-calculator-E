4090b47080a97e88eccee49361f79450b9b1a25d
Everett Butler <everettjamesbutler@gmail.com>
2023-03-03 14:18:17 -0600

    got my addition function working. Will be focusing on adding my history to the dom client side first, then the other functions, and then routing everything to the server.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,15 @@
 console.log('Yo waddup');
+
+const num1 = document.querySelector('#firstNumber').value;
+const num2 = document.querySelector('#secondNumber').value;
+
+function addNumbers(num1, num2) {
+    let sum = num1 + num2
+    return sum;
+}
+console.log(addNumbers(2,3));
+
+function submitEquation(event) {
+    event.preventDefault();
+    console.log('Successful submission');
+}
